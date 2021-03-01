cf2c9d3349babcff65aa740ca8720bcd8ae2bd74
exuanbo <exuanbo@protonmail.com>
2021-03-01 19:22:47 +0000

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,15 +2,11 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
-
-    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n % 2 == 0) {
+            return n / 2;
         } else {
-            return n * 2;
+            return 3 * n + 1;
         }
     }
 
@@ -24,4 +20,3 @@ public class Collatz {
         System.out.println();
     }
 }
-
